4aabc6e357edb46c195b29f74d5fdcc4c153a1cc
patriksimurka <patriksimurka@gmail.com>
2024-03-15 17:00:39 +0100

    Add training script

---
+++ models/equiformer_v2/train.py
@@ -0,0 +1,171 @@
+import argparse
+import datetime
+from typing import Iterable, Optional
+import torch
+import time
+from torch_geometric.loader import DataLoader
+from datasets.nbody.dataset_gravity import GravityDataset
+
+import wandb
+
+from models.equiformer_v2.architecture.equiformer_v2_nbody import EquiformerV2_nbody
+
+from timm.utils import ModelEmaV2, dispatch_clip_grad
+
+import torchmetrics
+
+ModelEma = ModelEmaV2
+
+
+
+def get_args_parser():
+    parser = argparse.ArgumentParser("Training EquiformerV2 on N-body", add_help=False)
+    parser.add_argument("--data-path", type=str, default="datasets/nbody")
+    parser.add_argument("--num-epochs", type=int, default=100)
+    parser.add_argument("--batch-size", type=int, default=16)
+    parser.add_argument("--lr", type=float, default=1e-4)
+    parser.add_argument("--max-samples", type=int, default=3000)
+    parser.add_argument("--dataset-name", type=str, default="nbody_small")
+    parser.add_argument("--log", type=bool, default=True)
+    parser.add_argument("--epochs", type=int, default=30)
+    parser.add_argument("--test-interval", type=int, default=1)
+    
+    return parser
+
+def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
+                    data_loader: Iterable, optimizer: torch.optim.Optimizer,
+                    device: torch.device, epoch: int,
+                    backprop: bool=True,
+                    args = None, 
+                    model_ema: Optional[ModelEma] = None,  
+                    loss_scaler=None,
+                    clip_grad=None,
+                    print_freq: int = 100, 
+                    logger=None):
+    
+    if backprop:
+        model.train()
+        criterion.train()
+    else:
+        model.eval()
+        criterion.eval()
+
+    res = {'epoch': epoch, 'counter': 0, 'mae': 0, 'loss': 0}
+    
+    loss_metric = torchmetrics.MeanMetric().to(device)
+    mae_metric = torchmetrics.MeanMetric().to(device)
+    
+    start_time = time.perf_counter()
+    
+    for batch_idx, batch_data in enumerate(data_loader):
+        data = [d.to(device) for d in batch_data]
+        data = [d.view(-1, d.size(2)) for d in data]
+        loc, vel, edge_attr, charges, loc_end = data
+        
+        # Create a Data object
+        data = (loc, vel, edge_attr, charges)
+        data = tuple(d.to(device) for d in data)
+        target = loc_end.to(device)
+        
+        pred = model(data, batch_idx)
+        pred = pred.squeeze()
+        
+        loss = criterion(pred, target)
+
+        if backprop:
+            optimizer.zero_grad()
+        if loss_scaler is not None:
+            loss_scaler(loss, optimizer, parameters=model.parameters())
+        else:
+            if backprop:
+                loss.backward()
+            if clip_grad is not None:
+                dispatch_clip_grad(model.parameters(), 
+                    value=clip_grad, mode='norm')
+            if backprop:
+                optimizer.step()
+        
+        loss_metric.update(loss)
+        err = pred.detach() - target
+        mae_metric.update(torch.mean(torch.abs(err)))
+    
+        if model_ema is not None:
+            model_ema.update(model)
+        
+        # torch.cuda.synchronize() TODO: do we need this?
+        
+        # logging
+        if batch_idx % print_freq == 0 or batch_idx == len(data_loader) - 1:
+            w = time.perf_counter() - start_time
+            e = (batch_idx + 1) / len(data_loader)
+            info_str = 'Epoch: [{epoch}][{step}/{length}] \t loss: {loss:.5f}, MAE: {mae:.5f}, time/step={time_per_step:.0f}ms, '.format( 
+                epoch=epoch, step=batch_idx, length=len(data_loader), 
+                mae=mae_metric.compute().item(), 
+                loss=loss_metric.compute().item(),
+                time_per_step=(1e3 * w / e / len(data_loader))
+                )
+            info_str += 'lr={:.2e}'.format(optimizer.param_groups[0]["lr"])
+            print(info_str)
+
+        res['counter'] += batch_data[0].size(0)
+        
+    return mae_metric.compute().item(), loss_metric.compute().item()
+
+
+def main(args):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    dataset_train = GravityDataset(partition='train', dataset_name=args.dataset_name,
+                                 max_samples=args.max_samples)
+    loader_train = torch.utils.data.DataLoader(dataset_train, batch_size=args.batch_size, shuffle=True, drop_last=True)
+
+    dataset_val = GravityDataset(partition='val', dataset_name=args.dataset_name)
+    loader_val = torch.utils.data.DataLoader(dataset_val, batch_size=args.batch_size, shuffle=False, drop_last=False)
+
+    dataset_test = GravityDataset(partition='test', dataset_name=args.dataset_name)
+    loader_test = torch.utils.data.DataLoader(dataset_test, batch_size=args.batch_size, shuffle=False, drop_last=False)
+
+    # Initialize model
+    model = (
+        EquiformerV2_nbody()
+    )  # Update irreps_in and irreps_out based on your dataset
+    model.to(device)
+
+    # Set up optimizer
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+
+    criterion = torch.nn.MSELoss()
+
+    wandb.init(project="EquiformerV2_nbody", config=args)
+
+
+    best_val_loss = 1e8
+    best_epoch = 0
+    for epoch in range(0, args.epochs):
+        print(f"Starting training epoch {epoch}")
+        _, train_loss = train_one_epoch(model, criterion, loader_train, optimizer, device, epoch, loader_train, args=args)
+        if args.log:
+            wandb.log({"Train MSE": train_loss})
+        if epoch % args.test_interval == 0 or epoch == args.epochs - 1:
+            print(f"Starting validation epoch {epoch}")
+            _, val_loss = train_one_epoch(model, criterion, loader_val, optimizer, device, epoch, backprop = False, args=args)
+            wandb.log({"Val MSE": val_loss})
+            
+            if val_loss < best_val_loss:
+                best_val_loss = val_loss
+                best_epoch = epoch
+            print("*** Best Val Loss: %.5f \t Best epoch %d" %
+                  (best_val_loss, best_epoch))
+            
+    current_time = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    torch.save(model.state_dict(), f"trained_models/{args.dataset_name}_best_model_{current_time}.pth")
+
+    return best_val_loss, best_epoch
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        "EquiformerV2 training script", parents=[get_args_parser()]
+    )
+    args = parser.parse_args()
+    main(args)
